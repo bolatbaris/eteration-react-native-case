eef5278979467a8cccc068bcc627bbaaa8af26f8
Baris BOLAT <bbarisbolat@gmail.com>
2024-01-11 20:38:24 +0300

    Add Bugsnag integration and Error Page

---
+++ src/error_page/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native'
+
+export default function ErrorPage() {
+  return (
+    <View>
+      <Text>Uygulamada bir hata gerçekleşti</Text>
+      <Text>
+        Ekibimiz hatayı yakaladı ve en kısa zamanda çözüme kavuşturacaktır
+      </Text>
+    </View>
+  )
+}
